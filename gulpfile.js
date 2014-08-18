3148a8706f3729693d10d940618e57224663291a
sangotaro <sango0715@gmail.com>
2014-08-18 19:36:48 +0900

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,13 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var plumber = require('gulp-plumber');
 var typescript  = require('gulp-tsc');
+var watch = require('gulp-watch');
 var mainBowerFiles  = require('main-bower-files');
 
 
 gulp.task('ts', function() {
   gulp.src(['app/scripts/index.ts'])
+    .pipe(plumber())
     .pipe(typescript({ out: 'main.js' }))
     .pipe(gulp.dest('public/'));
 });
@@ -15,15 +17,13 @@ gulp.task('vendor', function() {
   var files = mainBowerFiles();
   files.push('bower_components/EaselJS/lib/movieclip-0.7.1.combined.js');
   gulp.src(files)
-    .pipe(concat('vendor.js'))
     .pipe(plumber())
+    .pipe(concat('vendor.js'))
     .pipe(gulp.dest('public/'));
 });
 
-gulp.task('test', function() {
-  var files = mainBowerFiles();
-  files.push('bower_components/EaselJS/lib/movieclip-0.7.1.combined.js');
-  console.log(files);
+gulp.task('watch', function() {
+  gulp.watch(['bower_components/**/*.js'], ['vendor']);
 });
 
 gulp.task('build', ['vendor', 'ts']);
